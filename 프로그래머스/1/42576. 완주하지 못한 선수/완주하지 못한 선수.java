e6a829ef6e23416e29725847942ac862e8f61cd2
Hyunwoo Lee <106298918+Chehok@users.noreply.github.com>
2024-04-26 23:52:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 81.31 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,21 +1,23 @@
-import java.util.*;
+import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String str = "";
-        
-        Map<String, Integer> map = new HashMap<>();
-        for(String s: participant)
-            map.put(s, map.getOrDefault(s, 0) + 1);
-        for(String s: completion)
-            map.put(s, map.get(s) - 1);
+        HashMap<String, Integer> hm = new HashMap<>();
         
         for(String s: participant) {
-            if(map.get(s) != 0) {
-                str = s;
-                break;
+            hm.put(s, hm.getOrDefault(s, 0) + 1);
+        }
+        
+        for(String s: completion) {
+            hm.put(s, hm.get(s) - 1);
+        }
+        
+        for(String s: hm.keySet()) {
+            if(hm.get(s) == 1) {
+                return s;
             }
         }
-        return str;
+        
+        return null;
     }
 }
