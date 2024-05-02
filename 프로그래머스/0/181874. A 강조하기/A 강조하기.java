639f8347411b28e67417a9549868b1041c50a0df
Hyunwoo Lee <106298918+Chehok@users.noreply.github.com>
2024-05-02 22:26:41 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 67 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder answer = new StringBuilder();
+        myString = myString.toLowerCase();
+        
+        for(char c: myString.toCharArray()) {
+            if(c == 'a') {
+                answer.append('A');
+            } else {
+                answer.append(c);
+            }
+        }
+        
+        return answer.toString();
+    }
+}
