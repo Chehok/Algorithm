895b75e28fc0b104ff560adca8e456050772b2be
Hyunwoo Lee <106298918+Chehok@users.noreply.github.com>
2024-05-03 09:09:38 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.02 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int oddNum = 0;
+        int evenNum = 0;
+        
+        for(int i = 0; i < num_list.length; i++) {
+            if(i % 2 == 0) {
+                oddNum += num_list[i];
+            } else {
+                evenNum += num_list[i];
+            }
+        }
+        
+        return oddNum < evenNum ? evenNum : oddNum;
+    }
+}
