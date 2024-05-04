45b5cf0f1ea4e2634996524978052728e8605bf8
Hyunwoo Lee <106298918+Chehok@users.noreply.github.com>
2024-05-05 01:59:11 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.71 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,6 +1,6 @@
 import java.util.Queue;
 import java.util.PriorityQueue;
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 class Solution {
@@ -8,33 +8,38 @@ class Solution {
         int[] answer = new int[2];
         
         Queue<Integer> q1 = new PriorityQueue<>();
-        Queue<Integer> q2 = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<Integer> q2 = new LinkedList<>();
         
         for(String s: operations) {
             StringTokenizer st = new StringTokenizer(s);
             
             String temp = st.nextToken();
             if(temp.equals("I")) {
-                int val = Integer.valueOf(st.nextToken());
-                q1.add(val);
-                q2.add(val);
+                q1.add(Integer.valueOf(st.nextToken()));
             } else if (temp.equals("D")) {
                 if(q1.isEmpty()) continue;
                 temp = st.nextToken();
                 
                 if(temp.equals("-1")) {
-                    int min = q1.poll();
-                    q2.remove(min);
+                    q1.poll();
                 } else if (temp.equals("1")){
-                    int max = q2.poll();
-                    q1.remove(max);
+                    while(!q1.isEmpty() && q1.size() > 1) {
+                        q2.add(q1.poll());
+                    }
+                    q1.poll();
+                    while(!q2.isEmpty()) {
+                        q1.add(q2.poll());
+                    }
                 }
             }
         }
         
-        if(!q1.isEmpty()) {
-            answer[0] = q2.poll();
-            answer[1] = q1.poll();
+        if(!q1.isEmpty()) answer[1] = 1_000_000;
+        
+        while(!q1.isEmpty()) {
+            int now = q1.poll();
+            answer[0] = Math.max(answer[0], now);
+            answer[1] = Math.min(answer[1], now);
         }
         
         return answer;
