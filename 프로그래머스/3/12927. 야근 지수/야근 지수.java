a46d897014d1fe4872ec2bac846f657fb8ce2cfa
Hyunwoo Lee <106298918+Chehok@users.noreply.github.com>
2024-05-06 15:00:39 +0900

    [level 3] Title: 야근 지수, Time: 9.92 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,24 +1,20 @@
-import java.util.Collections;
 import java.util.Arrays;
 
 class Solution {
     public long solution(int n, int[] works) {
-        Integer[] _works = new Integer[works.length];
+        int len = works.length - 1;
         long answer = 0;
+        Arrays.sort(works);
         
-        for(int i = 0; i < works.length; i++) _works[i] = works[i];
-        Arrays.sort(_works, Collections.reverseOrder());
-        
-        while(n > 0 && _works[0] > 0) {
-            int idx = 0;
-            while (n > 0 && idx < works.length && _works[idx] >= _works[0]) {
-                _works[idx]--;
+        while(n > 0 && works[len] > 0) {
+            int idx = len;
+            while (n > 0 && 0 <= idx && works[idx] >= works[len]) {
+                works[idx--]--;
                 n--;
-                idx++;
             }
         }
         
-        for(int a: _works) {
+        for(int a: works) {
             answer += a * a;
         }
         return answer;
