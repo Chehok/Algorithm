dd9d8e675d83ab33264b07b5352e11f04fae3e16
Hyunwoo Lee <106298918+Chehok@users.noreply.github.com>
2024-05-09 16:33:05 +0900

    [level 3] Title: 등굣길, Time: 0.85 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] maps = new int[m + 1][n + 1];
+        maps[1][1] = 1;
+
+        for(int i = 1; i < m + 1; i++) {
+            loop: 
+            for(int j = 1; j < n + 1; j++) {
+                for(int[] p: puddles) {
+                    if(i == p[0] && j == p[1]) continue loop;
+                }
+                
+                maps[i][j] += (maps[i - 1][j] + maps[i][j - 1]) % 1_000_000_007;
+            }
+        }
+        
+        return maps[m][n] % 1_000_000_007;
+    }
+}
