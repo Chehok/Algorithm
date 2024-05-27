63b0bdf380a5aa0318d8920da6de6845aaa6aecc
Hyunwoo Lee <106298918+Chehok@users.noreply.github.com>
2024-05-27 14:25:28 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.57 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,23 +1,6 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
-
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
         int answer = 0;
-        List<int[]> list = new ArrayList<>();
-        
-        for(int i = 0; i < rank.length; i++) {
-            if(attendance[i])
-                list.add(new int[] {rank[i], i});
-        }
-        
-        Collections.sort(list, (o1, o2) -> (o1[0] - o2[0]));
-        
-        answer += list.get(0)[1] * 10000;
-        answer += list.get(1)[1] * 100;
-        answer += list.get(2)[1];
-        
         return answer;
     }
 }
