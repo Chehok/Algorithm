cb53336d7986397831211f617ed834f0bd5fd83d
Hyunwoo Lee <106298918+Chehok@users.noreply.github.com>
2024-06-12 17:10:55 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.30 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] arr = my_string.replaceAll("\\+ ", "").replaceAll("- ", "-").split(" ");
+        
+        for(String s: arr) {
+            answer += Integer.parseInt(s);
+        }
+        
+        return answer;
+    }
+}
